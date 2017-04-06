63559e50d8137f7d36870ff7b7004a9bd8b51610
severnsc <severnsc@gmail.com>
2017-04-06 16:43:27 -0400

    Create and complete tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,32 @@
+class TasksController < ApplicationController
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "Task created!"
+      redirect_to current_user
+    else
+      message = "Invalid task: "
+      @task.errors.full_messages.each do |error|
+        message += "#{error} "
+      end
+      flash[:danger] = message
+      redirect_to current_user
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if params[:task][:completed] == "true"
+      @task.update_attribute(:completed, true)
+      flash[:success] = "Task completed!"
+      redirect_to current_user
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :due_date)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :users, only: [:show]
+  resources :tasks
   root to: 'static_pages#home'
 end
