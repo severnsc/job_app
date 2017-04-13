c56c1b58d52a449db84d5e0f3d1bba64aac45ee8
severnsc <severnsc@gmail.com>
2017-04-12 20:46:51 -0400

    Update tests

--- test/models/reminder_test.rb
+++ test/models/reminder_test.rb
@@ -3,28 +3,15 @@ require 'test_helper'
 class ReminderTest < ActiveSupport::TestCase
   
   def setup
-    @reminder = Reminder.new(day_of_the_week: "Monday", time: Time.new(Date.today.year, Date.today.month, Date.today.day, 1, 0, 0), task: tasks(:task))
+    @reminder = Reminder.new(datetime: DateTime.now, task: tasks(:task))
   end
 
   test "should be valid" do
     assert @reminder.valid?
   end
 
-  test "should have day of the week" do
-    @reminder.day_of_the_week = " "
-    assert_not @reminder.valid?
-  end
-
-  test "day should be valid" do
-    invalid_days = %w[blahday moanday truesday wedsday thrisday frizday saturdal suunday]
-    invalid_days.each do |day|
-      @reminder.day_of_the_week = day
-      assert_not @reminder.valid?, "#{day} should not be valid"
-    end
-  end
-
-  test "should have time" do
-    @reminder.time = ""
+  test "should have date time" do
+    @reminder.datetime = " "
     assert_not @reminder.valid?
   end
 end
