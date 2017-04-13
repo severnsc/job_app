b49b3e377dc2964d487c633b33123999435064cd
severnsc <severnsc@gmail.com>
2017-04-12 21:41:35 -0400

    Add reminders for specific days when creating a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,22 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
+      @days = params[:days]
+      @days.each do |day, value|
+      if value == '1'
+        #Create a reminder for each weekday corresponding to the day selected at the time selected until the due date
+        datetime = DateTime.now + 1.day
+        time = "#{params[:"#{day}_time"]} #{params[:time_zone]}".to_time
+        until datetime >= params[:task][:due_date]
+          if datetime.strftime("%A").downcase == day
+            @task.reminders.create(datetime: datetime.change({hour: time.hour, minute: time.min}))
+            datetime += 7.days
+          else
+            datetime += 1.day
+          end
+        end
+      end
+    end
       flash[:success] = "Task created!"
       redirect_to current_user
     else
