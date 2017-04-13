2cd5e5b64042bf7c1cc0b3e19976f17ca4b869f0
severnsc <severnsc@gmail.com>
2017-04-12 21:07:37 -0400

    Add days and times to task creation form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
+    @task = @user.tasks.build
   end
 end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,6 +1,4 @@
 class Reminder < ApplicationRecord
   belongs_to :task
-  DAYS_OF_THE_WEEK = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday]
-  validates :day_of_the_week, presence: true, inclusion: {in: DAYS_OF_THE_WEEK}
-  validates :time, presence: true
+  validates :datetime, presence: true
 end
