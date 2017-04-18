c36bf6cb75a1514310706abbda231435b1de6ef5
severnsc <severnsc@gmail.com>
2017-04-18 14:22:26 -0400

    Add job model test

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ApplicationRecord
+  before_save :downcase_email
+
   belongs_to :user
   validates :position, presence: true
   validates :company, presence: true
@@ -6,4 +8,14 @@ class Job < ApplicationRecord
   validates :pay, presence: true
   validates :responsibilities, presence: true
   validates :requirements, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :contact_email, length: {maximum: 255},
+            format: { with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+
+  private
+
+  def downcase_email
+    contact_email.downcase!
+  end
 end
--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @job = Job.new(position: "position", company: "company", contact_name: "Name", contact_phone: "Phone", contact_email: "user@example.com", location: "somewhere", pay: "money", benefits: "some", responsibilities: "many", requirements: "a few", user: users(:user))
+  end
+
+  test "should be valid" do
+    assert @job.valid?
+  end
+
+  test "should have position" do
+    @job.position = "  "
+    assert_not @job.valid?
+  end
+
+  test "should have company" do
+    @job.company = " "
+    assert_not @job.valid?
+  end
+
+  test "should have location" do
+    @job.location = " "
+    assert_not @job.valid?
+  end
+
+  test "should have pay" do
+    @job.pay = " "
+    assert_not @job.valid?
+  end
+
+  test "should have responsibilities" do
+    @job.responsibilities = " "
+    assert_not @job.valid?
+  end
+
+  test "should have requirements" do
+    @job.requirements = " "
+    assert_not @job.valid?
+  end
+
+  test "contact email should save downcase" do
+    mixed_case_email = "UsEr@ExAmPlE.CoM"
+    @job.contact_email = mixed_case_email
+    @job.save
+    assert_equal mixed_case_email.downcase, @job.reload.contact_email
+  end
 end
